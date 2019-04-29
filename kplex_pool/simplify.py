0d6339282698affcc6c1d59a27f6e2e934eb09de
Francesco Landolfi <flandolfi@users.noreply.github.com>
2019-04-29 18:45:29 +0200

    Renae method

--- kplex_pool/simplify.py
+++ kplex_pool/simplify.py
@@ -2,12 +2,35 @@ import torch
 from kplex_pool import simplify_cpu
 
 
-def simplify(edge_index, weights, num_nodes=None, fair=True):
-    row, col = edge_index
+def simplify(edge_index, weights, keep_max=True, num_nodes=None, batch=None):
+    row, col = edge_index    
 
     if num_nodes is None:
         num_nodes = max(row.max().item(), col.max().item()) + 1
 
-    row, col, weights = simplify_cpu.remove_lightest(row, col, weights, num_nodes, fair)
+    if batch is None:
+        batch = row.new_zeros(num_nodes)
+        batch_size = 1
+    else:
+        batch_size = batch.max().item() + 1
     
-    return torch.stack([row, col], dim=0), weights
+    out_edges = []
+    out_weights = []
+
+    for b in range(batch_size):
+        node_mask = batch == b
+        edge_mask = node_mask.index_select(0, row)
+        r = row.masked_select(edge_mask)
+        c = col.masked_select(edge_mask)
+        w = weights.masked_select(edge_mask)
+        min_index = min(r.min().item(), c.min().item())
+        batch_nodes = max(r.max().item(), c.max().item()) + 1 - min_index
+        r = r.add(-min_index)
+        c = c.add(-min_index)
+
+        r, c, w = simplify_cpu.simplify_cutoff(r, c, w, batch_nodes, keep_max)
+
+        out_edges.append(torch.stack([r, c], dim=0).add(min_index))
+        out_weights.append(w)
+    
+    return torch.cat(out_edges, dim=1), torch.cat(out_weights, dim=0)
