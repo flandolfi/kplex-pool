bf8a1954d149ee96a3b9c83398e089672968ec9c
Francesco Landolfi <flandolfi@users.noreply.github.com>
2019-05-02 16:06:02 +0200

    Fix missing default parameter

--- kplex_pool/pool.py
+++ kplex_pool/pool.py
@@ -12,6 +12,9 @@ def cover_pool(x, edge_index, cover_index, edge_weights=None, cover_values=None,
     if edge_weights is None:
         edge_weights = torch.ones(edge_index[0].size(0), dtype=torch.float, device=edge_index.device)
 
+    if cover_values is None:
+        cover_values = torch.ones(cover_index[0].size(0), dtype=torch.float, device=cover_index.device)
+
     index_t, values_t = torch_sparse.transpose(cover_index, cover_values, num_nodes, num_clusters)
 
     out = torch_sparse.spmm(index_t, values_t, num_clusters, x)
