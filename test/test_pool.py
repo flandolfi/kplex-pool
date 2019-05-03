d00a7625fdf12d4599f1f2c0b48da2cc2076fdb6
Francesco Landolfi <flandolfi@users.noreply.github.com>
2019-05-03 12:08:37 +0200

    Add batch test

--- test/test_pool.py
+++ test/test_pool.py
@@ -5,6 +5,7 @@ from itertools import product
 from kplex_pool import kplex_cover
 from kplex_pool import cover_pool
 from kplex_pool.kplex_cpu import NodePriority
+from torch_geometric.data import Data, Batch
 
 
 devices = [torch.device('cpu')]
@@ -16,13 +17,20 @@ if torch.cuda.is_available():
     devices += [torch.device('cuda:{}'.format(torch.cuda.current_device()))]
 
 tests = [{
-        'row': [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3],  # clique
-        'col': [1, 2, 3, 0, 2, 3, 0, 1, 3, 0, 1, 2],
-        'k': 1
-    }, {
-        'row': [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4],  # 3-plex
-        'col': [1, 2, 3, 4, 0, 2, 3, 4, 0, 1, 3, 0, 1, 2, 0, 1],
-        'k': 3
+    'row': [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3],  # clique
+    'col': [1, 2, 3, 0, 2, 3, 0, 1, 3, 0, 1, 2],
+    'k': 1,
+    'cc': 1
+}, {
+    'row': [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4],  # 3-plex
+    'col': [1, 2, 3, 4, 0, 2, 3, 4, 0, 1, 3, 0, 1, 2, 0, 1],
+    'k': 3,
+    'cc': 1
+}, {
+    'row': [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 5], 
+    'col': [1, 2, 3, 0, 2, 3, 0, 1, 3, 0, 1, 2, 5, 4],
+    'k': 1,
+    'cc': 2
 }]
 
 
@@ -43,7 +51,7 @@ def test_cover_pool(test, cover_priority, kplex_priority, device):
         assert batch.size(0) == clusters
 
         if k == k_max:
-            assert x.size(0) == 1
+            assert x.size(0) == test['cc']
         
         index, values, nodes, clusters = kplex_cover(edge_index, k, None, True, cover_priority, kplex_priority)
         x = torch.ones((nodes, 1), dtype=torch.float, device=device)
@@ -55,4 +63,28 @@ def test_cover_pool(test, cover_priority, kplex_priority, device):
         assert batch.size(0) == clusters
 
         if k == k_max:
-            assert x.size(0) == 1
+            assert x.size(0) == test['cc']
+
+
+@pytest.mark.parametrize('cover_priority,kplex_priority,device',
+                         product(cover_priorities, kplex_priorities, devices))
+def test_cover_pool_batch(cover_priority, kplex_priority, device):
+    gs = []
+    ccs = 0
+    k = 0
+
+    for test in tests:
+        edge_index = torch.tensor([test['row'], test['col']], dtype=torch.long, device=device)
+        gs.append(Data(edge_index=edge_index))
+        ccs += test['cc']
+        k = max(k, test['k'])
+    
+    data = Batch.from_data_list(gs).to(device)
+    x = torch.ones((data.num_nodes, 1), dtype=torch.float, device=device)
+    index, values, nodes, clusters = kplex_cover(data.edge_index, k, None, False, cover_priority, kplex_priority)
+    x, idx, w, batch = cover_pool(x, data.edge_index, index, cover_values=values, batch=data.batch)
+
+    assert batch.max().item() + 1 == len(tests)
+    assert batch.size(0) == ccs
+    assert x.size(0) == ccs
+    
