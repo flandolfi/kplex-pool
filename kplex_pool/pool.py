a04004806505fa638a221b12aa64f12df3e05c1d
Francesco Landolfi <flandolfi@users.noreply.github.com>
2019-05-04 14:41:30 +0200

    Fix the ugly fix

--- kplex_pool/pool.py
+++ kplex_pool/pool.py
@@ -14,23 +14,26 @@ def cover_pool(x, edge_index, cover_index, edge_weights=None, cover_values=None,
 
     if cover_values is None:
         cover_values = torch.ones(cover_index[0].size(0), dtype=torch.float, device=cover_index.device)
+    
+    c_idx_t, c_val_t = torch_sparse.transpose(cover_index, cover_values, num_nodes, num_clusters)
 
-    # --- Ugly fix ---
-    # torch_sparse.spspmm produces different results if executed on GPU.
-    # Move all computation on CPU.
+    out = torch_sparse.spmm(c_idx_t, c_val_t, num_clusters, x)
+
+    #                       --- UGLY FIX ---                             #
+    # torch_sparse.spspmm produces different results if executed on GPU. #
+    # For now, I move all computation on CPU.                            #
+
     device = x.device
     c_idx = cover_index.cpu()
     c_val = cover_values.cpu()
     e_idx = edge_index.cpu()
     e_att = edge_weights.cpu()
-
-    c_idx_t, c_val_t = torch_sparse.transpose(c_idx, c_val, num_nodes, num_clusters)
-
-    out = torch_sparse.spmm(c_idx_t, c_val_t, num_clusters, x)
+    c_idx_t = c_idx_t.cpu()
+    c_val_t = c_val_t.cpu()
 
     out_adj_index, out_adj_weights = torch_sparse.spspmm(c_idx_t, c_val_t, e_idx, e_att, num_clusters,
                                                          num_nodes, num_nodes)
     out_adj_index, out_adj_weights = torch_sparse.spspmm(out_adj_index, out_adj_weights, c_idx, c_val,
                                                          num_clusters, num_nodes, num_clusters)
 
-    return out.to(device), out_adj_index.to(device), out_adj_weights.to(device)
+    return out, out_adj_index.to(device), out_adj_weights.to(device)
