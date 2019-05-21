853fda1291f4415b50cb50f9e2c86e3523d29e3d
Francesco Landolfi <flandolfi@users.noreply.github.com>
2019-05-21 11:50:50 +0200

    Add model parameters

--- benchmark/model.py
+++ benchmark/model.py
@@ -3,23 +3,29 @@ from math import ceil
 import torch
 import torch.nn.functional as F
 from torch.nn import Linear
-from torch_geometric.nn import GCNConv, global_max_pool, global_add_pool
+from torch_geometric.nn import GCNConv, SAGEConv, global_max_pool, global_add_pool
 
 from kplex_pool import kplex_cover, cover_pool_node, cover_pool_edge, simplify
 
 
 class KPlexPool(torch.nn.Module):
-    def __init__(self, dataset, num_layers, hidden, k, k_step_factor=0.5, simplify=False):
+    def __init__(self, dataset, num_layers, hidden, k, k_step_factor=0.5, 
+                 graph_sage=False, normalize=False, simplify=False):
         super(KPlexPool, self).__init__()
         self.k = k
         self.k_step_factor = k_step_factor
         self.simplify = simplify
+        self.normalize = normalize
+        self.graph_sage = graph_sage
 
-        self.conv_in = GCNConv(dataset.num_features, hidden)
+        feat = 1 if dataset.data.x is None else dataset.num_features
+        conv = SAGEConv if graph_sage else GCNConv
+
+        self.conv_in = conv(feat, hidden)
         self.blocks = torch.nn.ModuleList()
         
         for _ in range(num_layers - 1):
-            self.blocks.append(GCNConv(2 * hidden, hidden))
+            self.blocks.append(conv(2 * hidden, hidden))
         
         self.lin1 = Linear(2 * num_layers * hidden, hidden)
         self.lin2 = Linear(hidden, dataset.num_classes)
@@ -51,8 +57,12 @@ class KPlexPool(torch.nn.Module):
         weights = adj._values()
 
         batch_size = batch[-1].item() + 1
-        x = F.normalize(x)
-        x = F.relu(self.conv_in(x, edge_index, weights))
+
+        if self.graph_sage:
+            x = F.relu(self.conv_in(x, edge_index))
+        else:
+            x = F.relu(self.conv_in(x, edge_index, weights))
+
         xs = [ 
             global_add_pool(x, batch, batch_size), 
             global_max_pool(x, batch, batch_size)
@@ -62,8 +72,15 @@ class KPlexPool(torch.nn.Module):
 
         for block in self.blocks:    
             x, edge_index, weights, nodes, batch = self.pool_graphs(x, k, edge_index, weights, nodes, batch)
-            x = F.normalize(x)
-            x = F.relu(block(x, edge_index, weights))
+
+            if self.normalize:
+                x = F.normalize(x)
+
+            if self.graph_sage:
+                x = F.relu(block(x, edge_index))
+            else:
+                x = F.relu(block(x, edge_index, weights))
+            
             k = ceil(k*self.k_step_factor)
 
             xs.append(global_add_pool(x, batch, batch_size))
