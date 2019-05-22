64e30a750f779b75f43cc9ce0eebc64bee4b4521
Francesco Landolfi <flandolfi@users.noreply.github.com>
2019-05-22 14:51:08 +0200

    Remove LR scheduler

--- benchmark/search.py
+++ benchmark/search.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 
 import torch
-from torch.optim.lr_scheduler import StepLR
+
 from torch_geometric.datasets import TUDataset
 
 import json
@@ -13,7 +13,6 @@ import json
 import skorch
 from skorch import NeuralNetClassifier
 from skorch.dataset import CVSplit
-from skorch.callbacks import LRScheduler
 
 from benchmark.model import KPlexPool
 from kplex_pool.data import SkorchDataLoader, SkorchDataset
@@ -24,14 +23,12 @@ from sklearn.model_selection import GridSearchCV, StratifiedKFold
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str, default='PROTEINS')
-    parser.add_argument('--epochs', type=int, default=200)
+    parser.add_argument('--epochs', type=int, default=100)
     parser.add_argument('--batch_size', type=int, default=-1)
-    parser.add_argument('--lr', type=float, default=0.005)
-    parser.add_argument('--lr_decay_step', type=int, default=50)
-    parser.add_argument('--lr_decay_factor', type=float, default=0.5)
+    parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--weight_decay', type=float, default=0.001)
     parser.add_argument('--hidden', type=int, default=64)
-    parser.add_argument('--folds', type=int, default=5)
+    parser.add_argument('--folds', type=int, default=10)
     parser.add_argument('--max_k', type=int, default=16)
     parser.add_argument('--max_layers', type=int, default=4)
     parser.add_argument('--k_step_factor', type=float, default=0.5)
@@ -40,7 +37,7 @@ if __name__ == "__main__":
     parser.add_argument('--normalize', action='store_true')
     args = parser.parse_args()
 
-    dataset = TUDataset(root='/tmp/' + args.dataset, name=args.dataset)
+    dataset = TUDataset(root='data/' + args.dataset, name=args.dataset)
 
     net = NeuralNetClassifier(
         module=KPlexPool, 
@@ -57,11 +54,6 @@ if __name__ == "__main__":
         iterator_valid=SkorchDataLoader,
         dataset=SkorchDataset,
         train_split=None,
-        callbacks=[
-            ('lr_scheduler', LRScheduler(StepLR, 
-                                         step_size=args.lr_decay_step, 
-                                         gamma=args.lr_decay_factor))
-        ],
         device='cuda' if torch.cuda.is_available() else 'cpu'
     )
     
