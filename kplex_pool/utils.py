0dad82cea5c2a7b60644dbf1d9281a58e77229e9
Francesco Landolfi <flandolfi@users.noreply.github.com>
2019-06-10 15:17:51 +0200

    Add coverage and duplicates utility functions

--- kplex_pool/utils.py
+++ kplex_pool/utils.py
@@ -1,6 +1,9 @@
 import torch
 import numpy as np
+import torch_sparse
+
 from kplex_pool import cover_pool_node
+
 
 
 def pool_pos(pos: dict, cover_index: torch.LongTensor, num_clusters=None):
@@ -14,3 +17,31 @@ def pool_color(color: np.ndarray, cover_index: torch.LongTensor, num_clusters=No
     t_color = cover_pool_node(cover_index, t_color, num_clusters)
 
     return t_color.numpy().flatten()
+
+def count_duplicates(cover_index: torch.LongTensor, normalize=False):
+    num_nodes = cover_index[0].max().item() + 1
+    duplicates = cover_index.size(1) - num_nodes
+
+    if normalize:
+        duplicates /= num_nodes
+    
+    return duplicates
+
+def coverage(cover_index_list):
+    last_idx = cover_index_list[0]
+    last_val = torch.ones_like(last_idx[0], dtype=torch.float)
+    num_nodes = last_idx[0].max().item() + 1
+    num_clusters = last_idx[1].max().item() + 1
+
+    for mat in cover_index_list[1:]:
+        dim = mat[1].max().item() + 1
+        last_idx, last_val = torch_sparse.spspmm(last_idx, last_val,
+                                                 mat, torch.ones_like(mat[0], dtype=torch.float),
+                                                 num_nodes, num_clusters, dim)
+        num_clusters = dim
+    
+    last_val = torch.ones_like(last_val)
+    last_idx[0] = torch.zeros_like(last_idx[0])
+    _, coverage = torch_sparse.coalesce(last_idx, last_val, 1, num_clusters)
+
+    return coverage.numpy()/num_nodes
