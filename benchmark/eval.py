756c83c19f5d56c24753a841b5efb27a109fc435
Francesco Landolfi <flandolfi@users.noreply.github.com>
2019-07-24 18:55:32 +0200

    Add DiffPool and TopK models

--- benchmark/eval.py
+++ benchmark/eval.py
@@ -11,13 +11,14 @@ import skorch
 from skorch import NeuralNetClassifier
 from skorch.dataset import CVSplit
 
-from benchmark.model import KPlexPool
+from benchmark import model
 
 from sklearn.model_selection import StratifiedShuffleSplit
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default='KPlexPool')
     parser.add_argument('--dataset', type=str, default='PROTEINS')
     parser.add_argument('--cover_priority', type=str, default='default')
     parser.add_argument('--kplex_priority', type=str, default='default')
@@ -28,6 +29,7 @@ if __name__ == "__main__":
     parser.add_argument('--batch_size', type=int, default=-1)
     parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--weight_decay', type=float, default=0.001)
+    parser.add_argument('--ratio', type=float, default=0.8)
     parser.add_argument('--split', type=float, default=0.1)
     parser.add_argument('--layers', type=int, default=3)
     parser.add_argument('--hidden', type=int, default=64)
@@ -51,22 +53,10 @@ if __name__ == "__main__":
     y = dataset.data.y.numpy()
 
     net = NeuralNetClassifier(
-        module=KPlexPool, 
+        module=getattr(model, args.model), 
         module__dataset=dataset,
         module__num_layers=args.layers,
         module__hidden=args.hidden,
-        module__k=args.k if args.ks is None else args.ks,
-        module__k_step_factor=args.k_step_factor,
-        module__graph_sage=args.graph_sage,
-        module__normalize=args.normalize,
-        module__readout=args.no_readout,
-        module__skip_covered=args.skip_covered,
-        module__cache_results=args.no_cache,
-        module__cover_priority=args.cover_priority,
-        module__kplex_priority=args.kplex_priority,
-        module__global_pool_op=args.global_pool_op,
-        module__node_pool_op=args.node_pool_op,
-        module__edge_pool_op=args.edge_pool_op,
         max_epochs=args.epochs,
         batch_size=args.batch_size,
         lr=args.lr,
@@ -76,5 +66,23 @@ if __name__ == "__main__":
         train_split=CVSplit(cv=StratifiedShuffleSplit(test_size=args.split, n_splits=1, random_state=42)),
         device='cuda' if torch.cuda.is_available() else 'cpu'
     )
+
+    if args.model == 'KPlexPool':
+        net.set_params(
+            module__k=args.k if args.ks is None else args.ks,
+            module__k_step_factor=args.k_step_factor,
+            module__graph_sage=args.graph_sage,
+            module__normalize=args.normalize,
+            module__readout=args.no_readout,
+            module__skip_covered=args.skip_covered,
+            module__cache_results=args.no_cache,
+            module__cover_priority=args.cover_priority,
+            module__kplex_priority=args.kplex_priority,
+            module__global_pool_op=args.global_pool_op,
+            module__node_pool_op=args.node_pool_op,
+            module__edge_pool_op=args.edge_pool_op
+        )
+    else:
+        net.set_params(module__ratio=args.ratio)
 
     net.fit(X, y)
