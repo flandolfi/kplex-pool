00e9da8113d600cad65c579a98c462363f12901d
Francesco Landolfi <flandolfi@users.noreply.github.com>
2019-07-29 16:59:34 +0200

    Update model parameters

--- benchmark/eval.py
+++ benchmark/eval.py
@@ -25,13 +25,16 @@ if __name__ == "__main__":
     parser.add_argument('--global_pool_op', type=str, default='add')
     parser.add_argument('--node_pool_op', type=str, default='add')
     parser.add_argument('--edge_pool_op', type=str, default='add')
+    parser.add_argument('--jumping_knowledge', type=str, default='cat')
     parser.add_argument('--epochs', type=int, default=100)
     parser.add_argument('--batch_size', type=int, default=-1)
     parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--weight_decay', type=float, default=0.001)
+    parser.add_argument('--dropout', type=float, default=0.3)
     parser.add_argument('--ratio', type=float, default=0.8)
     parser.add_argument('--split', type=float, default=0.1)
     parser.add_argument('--layers', type=int, default=3)
+    parser.add_argument('--inner_layers', type=int, default=2)
     parser.add_argument('--hidden', type=int, default=64)
     parser.add_argument('--k', type=int, default=8)
     parser.add_argument('--k_step_factor', type=float, default=0.5)
@@ -52,26 +55,29 @@ if __name__ == "__main__":
     X = np.arange(len(dataset)).reshape((-1, 1))
     y = dataset.data.y.numpy()
 
-    net = NeuralNetClassifier(
-        module=getattr(model, args.model), 
-        module__dataset=dataset,
-        module__num_layers=args.layers,
-        module__hidden=args.hidden,
-        max_epochs=args.epochs,
-        batch_size=args.batch_size,
-        lr=args.lr,
-        optimizer=torch.optim.Adam,
-        optimizer__weight_decay=args.weight_decay,
-        iterator_train__shuffle=True,
-        train_split=CVSplit(cv=StratifiedShuffleSplit(test_size=args.split, n_splits=1, random_state=42)),
-        device='cuda' if torch.cuda.is_available() else 'cpu'
-    )
+    params = {
+        'module': getattr(model, args.model), 
+        'module__dataset': dataset,
+        'module__num_layers': args.layers,
+        'module__hidden': args.hidden,
+        'module__graph_sage': args.graph_sage,
+        'module__dropout': args.dropout,
+        'module__num_inner_layers': args.inner_layers,
+        'module__jumping_knowledge': args.jumping_knowledge,
+        'max_epochs': args.epochs,
+        'batch_size': args.batch_size,
+        'lr': args.lr,
+        'optimizer': torch.optim.Adam,
+        'optimizer__weight_decay': args.weight_decay,
+        'iterator_train__shuffle': True,
+        'train_split': CVSplit(cv=StratifiedShuffleSplit(test_size=args.split, n_splits=1, random_state=42)),
+        'device': 'cuda' if torch.cuda.is_available() else 'cpu'
+    }
 
     if args.model == 'KPlexPool':
-        net.set_params(
+        params.update(
             module__k=args.k if args.ks is None else args.ks,
             module__k_step_factor=args.k_step_factor,
-            module__graph_sage=args.graph_sage,
             module__normalize=args.normalize,
             module__readout=args.no_readout,
             module__skip_covered=args.skip_covered,
@@ -83,6 +89,6 @@ if __name__ == "__main__":
             module__edge_pool_op=args.edge_pool_op
         )
     else:
-        net.set_params(module__ratio=args.ratio)
+        params.update(module__ratio=args.ratio)
 
-    net.fit(X, y)
+    NeuralNetClassifier(**params).fit(X, y)
