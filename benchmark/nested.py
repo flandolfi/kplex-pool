89fb9d601020d5cbbb07412151406613f505fea6
Francesco Landolfi <flandolfi@users.noreply.github.com>
2019-08-06 12:37:23 +0200

    Update default parameter

--- benchmark/nested.py
+++ benchmark/nested.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     parser.add_argument('--dataset', type=str, default='PROTEINS')
     parser.add_argument('--jumping_knowledge', type=str, default='cat')
     parser.add_argument('--max_epochs', type=int, default=1000)
-    parser.add_argument('--patience', type=int, default=10)
+    parser.add_argument('--patience', type=int, default=20)
     parser.add_argument('--batch_size', type=int, default=-1)
     parser.add_argument('--dropout', type=float, default=0.3)
     parser.add_argument('--inner_folds', type=int, default=10)
