015ddb0eb410de968394fc54469703f1953baff9
Francesco Landolfi <flandolfi@users.noreply.github.com>
2019-09-13 22:34:48 +0200

    Add DenseDataset

--- kplex_pool/data.py
+++ kplex_pool/data.py
@@ -1,5 +1,8 @@
 import torch
-from torch_geometric.data import Data, InMemoryDataset
+from torch.utils.data.dataloader import default_collate
+
+from torch_geometric.data import Data, Batch, InMemoryDataset, Dataset
+from torch_geometric.transforms import ToDense
 
 
 
@@ -50,6 +53,65 @@ class CustomDataset(InMemoryDataset):
         super(CustomDataset, self).__init__("")
         self.data, self.slices = self.collate(data_list)
     
+    def _download(self):
+        pass
+
+    def _process(self):
+        pass
+
+
+class DenseDataset(Dataset):
+    def __init__(self, data_list):
+        super(DenseDataset, self).__init__("")
+
+        self.data = Batch()
+        max_nodes = max([data.num_nodes for data in data_list])
+        to_dense = ToDense(max_nodes)
+        dense_list = [to_dense(data) for data in data_list]
+
+        if 'cover_index' in data_list[0]:
+            max_clusters = max([data.num_clusters for data in data_list])    
+
+            for data in dense_list:
+                data.cover_mask = torch.zeros(max_clusters, dtype=torch.uint8)
+                data.cover_mask[:data.num_clusters] = 1  
+                data.cover_index = torch.sparse_coo_tensor(
+                        indices=data.cover_index,
+                        values=torch.ones_like(data.cover_index[0]), 
+                        size=torch.Size([max_nodes, max_clusters]),
+                        dtype=torch.float
+                    ).to_dense()
+
+        for key in dense_list[0].keys:
+            self.data[key] = default_collate([d[key] for d in dense_list])
+
+    def __len__(self):
+        if self.data.x is not None:
+            return self.data.x.size(0)
+
+        if self.data.adj is not None:
+            return self.data.adj.size(0)
+
+        return 0
+
+    def get(self, idx):
+        mask = self.data.mask[idx]
+        max_nodes = mask.argmax(-1).max().item() + 1
+        out = Batch()
+
+        for key, item in self.data('x', 'pos', 'y', 'mask'):
+            out[key] = item[idx, :max_nodes]
+
+        out.adj = self.data.adj[idx, :max_nodes, :max_nodes]
+        
+        if 'cover_index' in self.data:
+            cover_mask = self.data.cover_mask[idx]
+            max_clusters = cover_mask.argmax(-1).max().item() + 1
+            out.cover_index = self.data.cover_index[idx, :max_nodes, :max_clusters]
+            out.cover_mask = self.data.cover_mask[idx, :max_clusters]
+
+        return out
+
     def _download(self):
         pass
 
