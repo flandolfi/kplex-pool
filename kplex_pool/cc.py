a8be1eec2dddafe80e36c9ae04b374061d7130c1
Francesco Landolfi <flandolfi@users.noreply.github.com>
2020-02-15 18:22:28 +0100

    Add docstrings

--- kplex_pool/cc.py
+++ kplex_pool/cc.py
@@ -3,6 +3,15 @@ import torch_sparse
 from kplex_pool import kplex_cpu, cc_cpu
 
 def connected_components(edge_index, num_nodes=None):
+    """Find the connected components of a given graph.
+    
+    Args:
+        edge_index (LongTensor): Edge coordinate matrix.
+        num_nodes (int, optional): Number of nodes. Defaults to None.
+    
+    Returns:
+        LongTensor: Vector assigning each node to its component index.
+    """
     if num_nodes is None:
         num_nodes = edge_index.max().item() + 1
 
