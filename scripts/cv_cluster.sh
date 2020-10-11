e5e7754dde93053d9b915799e84e1df4d30ba446
Francesco Landolfi <flandolfi@users.noreply.github.com>
2020-10-11 18:04:16 +0200

    Update benchmark scripts

--- scripts/cv_cluster.sh
+++ scripts/cv_cluster.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-for POOL in BaseModel TopKPool SAGPool; do
-   for DS in DD PROTEINS ENZYMES NCI1 IMDB-BINARY IMDB-MULTI; do
+for POOL in Louvain Leiden ECG Graclus; do
+   for DS in IMDB-BINARY IMDB-MULTI; do
       python benchmark/cv.py --model=$POOL --dataset=$DS --to_pickle=results/${POOL}_${DS}.pickle
+   done
+
+   for DS in DD PROTEINS ENZYMES NCI1; do
+       python benchmark/cv.py --model=$POOL --dataset=$DS --to_pickle=results/${POOL}_${DS}_ADDMAX.pickle --node_pool_op add max
    done
 
    for DS in COLLAB REDDIT-BINARY REDDIT-MULTI-5K; do
       python benchmark/cv.py --model=$POOL --dataset=$DS --batch_size=1000 --to_pickle=results/${POOL}_${DS}.pickle
    done
 done
+
