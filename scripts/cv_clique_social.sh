d3b9eecaf2f4445eb5b1f4e6d39d073a6b06d8f1
Francesco Landolfi <flandolfi@users.noreply.github.com>
2020-12-31 17:07:36 +0100

    Sparse CliquePool benchmarks

--- scripts/cv_clique_social.sh
+++ scripts/cv_clique_social.sh
@@ -3,18 +3,12 @@
 POOL=CliquePool
 ARGS="--model=${POOL} --max_layers=2"
 
-for DS in REDDIT-BINARY REDDIT-MULTI-5K; do
-   python benchmark/cv.py $ARGS --dataset=$DS --to_pickle=results/${POOL}_${DS}.pickle --batch_size=20 --dense --dense_from=1
+for DS in COLLAB REDDIT-BINARY REDDIT-MULTI-5K; do
+   python benchmark/cv.py $ARGS --dataset=$DS --to_pickle=results/${POOL}_${DS}.pickle --batch_size=1000
 done
 
 ARGS="$ARGS --dense"
 
-for DS in IMDB-BINARY IMDB-MULTI COLLAB; do
-    if [ $DS = COLLAB ]; then
-        OPT="--batch_size=1000"
-    else
-        OPT=""
-    fi
-
+for DS in IMDB-BINARY IMDB-MULTI; do
     python benchmark/cv.py $ARGS $OPT --dataset=$DS --to_pickle=results/${POOL}_${DS}.pickle
 done
