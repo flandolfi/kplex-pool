38034fdd00237bd2484c60a55264c812b88c5757
Francesco Landolfi <flandolfi@users.noreply.github.com>
2020-12-31 19:01:25 +0100

    Fix batch size

--- scripts/cv_clique_social.sh
+++ scripts/cv_clique_social.sh
@@ -4,7 +4,7 @@ POOL=CliquePool
 ARGS="--model=${POOL} --max_layers=2"
 
 for DS in REDDIT-BINARY REDDIT-MULTI-5K COLLAB; do
-   python benchmark/cv.py $ARGS --dataset=$DS --to_pickle=results/${POOL}_${DS}.pickle --batch_size=1000
+   python benchmark/cv.py $ARGS --dataset=$DS --to_pickle=results/${POOL}_${DS}.pickle --batch_size=500
 done
 
 ARGS="$ARGS --dense"
